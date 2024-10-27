749d2f957d1dd2f74ac3ae11156be388e5e919aa
JJ807 <jakubstrugaa@gmail.com>
2024-10-27 16:03:56 +0100

    add 2nd solution

--- best-time-to-buy-and-sell-stock/Solution.java
+++ best-time-to-buy-and-sell-stock/Solution.java
@@ -1,8 +1,5 @@
 public class Solution {
     public static int maxProfit(int[] prices) {
-        // znalezienie najwiekszej roznicy pomiedzy i-ta liczba a kolejnymi, pod warunkiem ze reszta jest wieksza od itej,
-        // zapisanie tej roznicy do tablicy elemenotw
-        // zwrocenie maxa z tej tablicy za pomoca Collections.max(), chyba ze max jest <0 to zwrocenie 0
         int length = prices.length;
         int maxProfit = 0;
         for(int i=0; i<length; ++i) {
@@ -16,8 +13,24 @@ public class Solution {
          return maxProfit;
     }
 
+    public static int maxProfit2(int[] prices) {
+       int maxProfit = 0;
+       int minPrice = Integer.MAX_VALUE;
+       for(int price: prices) {
+        if(price < minPrice) {
+            minPrice = price;
+        }
+        int profit = price - minPrice;
+        if(profit > maxProfit) {
+            maxProfit = profit;
+        }
+       }
+       return maxProfit;
+    }
+
     public static void main(String[] args) { 
-        int[] prices = new int[] {7,6,4,3,1};
-        System.out.println(maxProfit(prices));
+        int[] prices = new int[] {7, 1, 5, 3, 6, 4};
+        System.out.println("sol1: " + maxProfit(prices));
+        System.out.println("sol2: " + maxProfit2(prices));
     }
 }
